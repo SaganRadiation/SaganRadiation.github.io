ca087102189777017a42b9cfb8ea0b71ea657d2a
saganradiation <jakegrimes9@gmail.com>
2020-06-06 11:40:53 -0400

    fill in a background

--- games/tictactoe/js/game.js
+++ games/tictactoe/js/game.js
@@ -1,1 +1,8 @@
-alert('Javascript loaded')
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
